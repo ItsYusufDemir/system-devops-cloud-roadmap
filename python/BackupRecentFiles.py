e1364f74023804045b2c3881d2987d289e246aed
ItsYusufDemir <itsyusufdemir@gmail.com>
2025-11-28 21:01:43 +0300

    2 exercises added

--- python/BackupRecentFiles.py
+++ python/BackupRecentFiles.py
@@ -10,4 +10,87 @@ Create a script that:
 Checks a folder
 
 Copies only files modified in the last 24 hours into a backup/ folder
+
+
 """
+
+import sys
+import os
+import time
+import shutil
+
+LAST_ACCESS_TIME = 3600 * 24 # SECONDS
+BACKUP_PATH = "python/backup"  # ./backup in current dir
+
+counter = 0
+
+
+
+
+def backup(folder_path):
+
+    accessed_files = []
+
+    global counter
+
+
+    try:
+
+
+        dir_list = os.listdir(folder_path)
+
+        current_time = time.time()
+
+        for i in dir_list:
+            full_path = os.path.join(folder_path, i)
+
+            if (os.path.isdir(full_path)):
+                backup(full_path)
+                continue
+
+            access_time = os.path.getmtime(full_path)
+            
+            if (current_time - access_time < LAST_ACCESS_TIME):
+                counter += 1
+                accessed_files.append(full_path)
+
+
+
+
+        if (len(accessed_files) == 0):
+            return
+
+        
+
+
+        for file_path in accessed_files:
+            shutil.copy(file_path, BACKUP_PATH)
+
+
+          
+    except FileNotFoundError:
+        print(f"Path {folder_path} does not exist!")
+    except NotADirectoryError:
+        print(f"Path {folder_path} is not a directory!")
+    except PermissionError:
+        print(f"Path {folder_path} cannot access to the directory")
+
+
+try:
+    folder_path = sys.argv[1]
+
+
+    os.makedirs(BACKUP_PATH, exist_ok=True)
+    print("Starting backup...")
+
+    backup(folder_path)
+
+    print(f"{counter} files has been modified in the last 24 hours")
+    print("Backup completed.")
+
+
+
+except Exception as e:
+    print("Usage: BackupRecentFiles.py <path-to-folder>",e)
+    sys.exit(1)
+
--- python/LogFileAnalyzer.py
+++ python/LogFileAnalyzer.py
@@ -9,9 +9,45 @@ Write a script that:
 
 Reads a .log file
 
-Counts how many times the word “ERROR” appears
+Counts how many times the word "error" appears
 
 Prints the total count
 
 🧠 Why: This helps you automate server health monitoring.
 """
+
+import sys
+
+try:
+    log_file = sys.argv[1] # argv[0] is this python file path/name, so our real argv is in index 1
+
+    try:
+        file = open(log_file)
+
+        row_number = 1
+        line = file.readline()
+        while line:
+            line = line.lower()
+            condition = line.find("error")
+
+            if (condition != -1):
+                print(f"\033[1;34m{row_number}:{condition}\033[0m || \033[1;92m{line}\033[0m")
+                
+
+            row_number += 1
+            line = file.readline()
+
+
+    except:
+        print("Log file couldn't opened")
+
+except: 
+    print("Usage: python3 LogFileAnalyzer.py <log-file>")
+
+
+
+
+
+
+
+
